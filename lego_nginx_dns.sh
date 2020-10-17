e5722b2da2a6474de760f33489affd0ea3fb3a50
Cheni Chadowitz <c.chadowitz@gmail.com>
2020-10-17 18:53:00 -0400

    Update lego_nginx_dns.sh
    Bugfix for quotes

--- lego_nginx_dns.sh
+++ lego_nginx_dns.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ ! -f /etc/nginx/ssl/${DOMAIN}/${DOMAIN}.crt ]]; then
   mkdir -p /etc/nginx/ssl/${DOMAIN}
-  /opt/lego --dns cloudflare --domains '${DOMAIN}' --domains '*.${DOMAIN}' --email ${EMAIL} --accept-tos run --run-hook "./install_certs.sh && nginx -t"
+  /opt/lego --dns cloudflare --domains "${DOMAIN}" --domains "*.${DOMAIN}" --email ${EMAIL} --accept-tos run --run-hook "./install_certs.sh && nginx -t"
   if [[ "$?" != "0" ]]; then
     exit 2;
   fi
@@ -15,7 +15,7 @@ fi
 
 
 
-$(while :; do /opt/lego --dns cloudflare --domains '${DOMAIN}' --domains '*.${DOMAIN}' --email ${EMAIL} renew --days 45 --renew-hook "./install_certs.sh && nginx -t && nginx -s reload"; sleep "${RENEW_INTERVAL:-12h}"; done;) &
+$(while :; do /opt/lego --dns cloudflare --domains "${DOMAIN}" --domains "*.${DOMAIN}" --email ${EMAIL} renew --days 45 --renew-hook "./install_certs.sh && nginx -t && nginx -s reload"; sleep "${RENEW_INTERVAL:-12h}"; done;) &
 
 
 nginx -g "daemon off;"

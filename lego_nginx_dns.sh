59934294f8939a1e4341623d914bf7b2970d8ba9
Cheni Chadowitz <c.chadowitz@gmail.com>
2021-02-18 16:03:51 -0500

    Update lego_nginx_dns.sh
    Fixed path to install_certs.sh

--- lego_nginx_dns.sh
+++ lego_nginx_dns.sh
@@ -5,14 +5,14 @@ if [[ -z "${DOMAIN}" || -z "${EMAIL}" ]]; then
   exit 1;
 fi
 
-lego_renew=" --dns cloudflare --domains=${DOMAIN} --domains=*.${DOMAIN} --email=${EMAIL} renew --days=45 --renew-hook=\"./install_certs.sh && nginx -t && nginx -s reload\""                                                                
+lego_renew=" --dns cloudflare --domains=${DOMAIN} --domains=*.${DOMAIN} --email=${EMAIL} renew --days=45 --renew-hook=\"/opt/install_certs.sh && nginx -t && nginx -s reload\""                                                                
 /opt/lego ${lego_renew}
 exit_code=$?
 echo "exit code ${exit_code}"
 
 if [[ ! -f /etc/nginx/ssl/${DOMAIN}/${DOMAIN}.crt || ${exit_code} -ne 0 ]]; then
   mkdir -p /etc/nginx/ssl/${DOMAIN}
-  /opt/lego --dns cloudflare --domains=${DOMAIN} --domains=*.${DOMAIN} --email=${EMAIL} --accept-tos run --run-hook="./install_certs.sh && nginx -t"                                                                
+  /opt/lego --dns cloudflare --domains=${DOMAIN} --domains=*.${DOMAIN} --email=${EMAIL} --accept-tos run --run-hook="/opt/install_certs.sh && nginx -t"                                                                
   if [[ "$?" != "0" ]]; then
     exit 2;
   fi

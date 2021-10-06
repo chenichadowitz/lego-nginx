8e54377644db2e5a65f9a747af6224bca8d65b33
Cheni Chadowitz <c.chadowitz@gmail.com>
2021-10-06 16:00:26 -0400

    Update install_certs.sh
    add nginx config test + reload in the script directly

--- install_certs.sh
+++ install_certs.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/sh -x
 
-cp ${LEGO_CERT_PATH} /etc/nginx/ssl/${LEGO_CERT_DOMAIN}/${DOMAIN}.crt
-cp ${LEGO_CERT_KEY_PATH} /etc/nginx/ssl/${LEGO_CERT_DOMAIN}/${DOMAIN}.key
+cp ${LEGO_CERT_PATH} /etc/nginx/ssl/${LEGO_CERT_DOMAIN}/${DOMAIN}.crt && \
+cp ${LEGO_CERT_KEY_PATH} /etc/nginx/ssl/${LEGO_CERT_DOMAIN}/${DOMAIN}.key && \
+nginx -t && \
+nginx -s reload

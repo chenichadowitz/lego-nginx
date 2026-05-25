805fa45d8f061f8fe7536a60db12c85842195b2f
Cheni Chadowitz <c.chadowitz@gmail.com>
2026-05-24 22:12:23 -0400

    Update lego_nginx.sh

--- lego_nginx.sh
+++ lego_nginx.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ ! -f /etc/nginx/ssl/${DOMAIN}/${DOMAIN}.crt ]]; then
   mkdir -p /etc/nginx/ssl/${DOMAIN}
-  /opt/lego --email ${EMAIL} --accept-tos run -d ${DOMAIN} --tls --tls.port :${LEGO_TLS_PORT} --run-hook "./install_certs.sh && nginx -t"
+  /opt/lego run -d ${DOMAIN} --tls --tls.port :${LEGO_TLS_PORT} --email ${EMAIL} --accept-tos --run-hook "./install_certs.sh && nginx -t"
   if [[ "$?" != "0" ]]; then
     exit 2;
   fi
